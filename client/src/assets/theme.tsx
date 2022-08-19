a8b0b58485cdb9fa676ae45c01cb15a0800822c8
halecolin1 <colin@invest.inc>
2022-08-18 20:41:16 -0600

    Starting theme for material io

---
+++ client/src/assets/theme.tsx
@@ -0,0 +1,4 @@
+export const theme = {
+  primary: "#ba68c8",
+  secondary: "#80deea",
+};
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ba68c8",
+    },
+    secondary: {
+      main: "#80deea",
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
